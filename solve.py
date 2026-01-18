df0005801829fc3e22341458278533cc39f95767
Boyyeo <87563842+Boyyeo@users.noreply.github.com>
2026-01-18 22:32:55 +0800

    Add files via upload

---
+++ solve.py
@@ -0,0 +1,64 @@
+
+
+import torch 
+
+
+
+## module choleski
+''' L = choleski(a)
+Choleski decomposition: [L][L]transpose = [a]
+x = choleskiSol(L,b)
+Solution phase of Choleski's decomposition method
+'''
+import numpy as np
+import math
+
+
+def choleski(a):
+    n = len(a)
+    for k in range(n):
+        try:
+            a[k,k] = torch.sqrt(a[k,k] - torch.dot(a[k,0:k],a[k,0:k]))
+        except ValueError:
+            print('Matrix is not positive definite')
+        for i in range(k+1,n):
+            a[i,k] = (a[i,k] - torch.dot(a[i,0:k],a[k,0:k]))/a[k,k]
+    for k in range(1,n): a[0:k,k] = 0.0
+    return a
+
+def choleskiSol(b,L):
+    print('L shape:{} b shape:{}'.format(L.shape, b.shape))
+    n = len(b)
+    # Solution of [L]{y} = {b}
+    for k in range(n):
+        b[k] = (b[k] - torch.dot(L[k,0:k],b[0:k]))/L[k,k]
+    # Solution of [L_transpose]{x} = {y}
+    for k in range(n-1,-1,-1):
+        b[k] = (b[k] - torch.dot(L[k+1:n,k],b[k+1:n]))/L[k,k]
+    return b
+
+
+ckpt = torch.load('/nfs/home/boyyeo/mediatek/alpamayo/src/alpamayo_r1/debug_dxy_theta_to_v_without_v0.pt')
+lhs = ckpt['lhs']
+rhs = ckpt['rhs']
+L = ckpt['L']       
+y = ckpt['y']
+
+print('lhs shape:{} rhs shape:{} L shape:{} y shape:{}'.format(lhs.shape, rhs.shape, L.shape, y.shape))
+
+#L_recomputed = cholesky_decomposition(lhs)
+print('lhs:', lhs.shape)
+L_recomputed = choleski(lhs.clone().squeeze())
+#print('L_recomputed:', L_recomputed)
+#torch.linalg.cholesky(lhs)
+print('L and L_recomputed close:', torch.allclose(L, L_recomputed))  
+print('difference between L and L_recomputed:', (L - L_recomputed).abs().max())   
+
+
+#y_recomputed = torch.cholesky_solve(rhs.unsqueeze(-1), L).squeeze(-1)
+print('L shape:', L.shape)
+print('L_recomputed shape:', L_recomputed.shape)
+print('rhs shape:', rhs.shape)
+y_recomputed = choleskiSol(rhs.squeeze(), L.squeeze())
+print('y and y_recomputed close:', torch.allclose(y, y_recomputed))
+print('difference between y and y_recomputed:', (y - y_recomputed).abs().max())
